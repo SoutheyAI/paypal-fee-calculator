8dc2366481605cca04ad7f7b6398af3fcffb8283
JavaNb666 <1176499766@qq.com>
2024-12-09 19:52:30 +0800

    fix: change output to static for Vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  output: 'static',
   site: 'https://feecalc.org',
   integrations: [
     tailwind(),
